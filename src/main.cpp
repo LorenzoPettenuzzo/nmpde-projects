801438ef997121c7c245be176500db02bb675bc9
Lorenzo Pettenuzzo <lorenzo.pettenuzzo02@gmail.com>
2025-05-08 14:30:22 +0200

    Simulation Timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include "LinearFisherKolmogorov.hpp"
 #include "FisherKolmogorov.hpp"
 
+#include <chrono>
+
 int main(int argc, char *argv[]) {
 
     Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
 
+    freopen("log.txt", "w", stdout);
+
     //LinearFisherKolmogorov<3> lfk("../mesh/brain-h3.0.msh", 1, 10, 0.01);
     //LinearFisherKolmogorov<3> lfk("../mesh/brain-h3.0.msh", 1, 4, 0.25);
-    FisherKolmogorov<3> lfk("../mesh/brain-h3.0.msh", 1, 30, 0.5);
+
+    auto t_start = std::chrono::high_resolution_clock::now();
+
+    FisherKolmogorov<3> lfk("../mesh/brain-h3.0.msh", 1, 2, 1);
     lfk.setup();
     lfk.solve();
+
+    auto t_end = std::chrono::high_resolution_clock::now();
+
+    auto t_m = std::chrono::duration_cast<std::chrono::minutes>(t_end - t_start);
+    auto t_s = std::chrono::duration_cast<std::chrono::seconds>(t_end - t_start);
+
+    std::cout << t_m.count() << " minutes " << t_s.count() % 60 << " seconds" << std::endl;
 }
